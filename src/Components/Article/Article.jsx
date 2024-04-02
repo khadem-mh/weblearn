4768e799b6731f4ab081cdde8b43a7ef2d4000df
khadem-mh <khadem13359@gmail.com>
2024-04-02 13:02:30 +0330

    added loader from image article box

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Article.css'
+import { Link } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 import { FaArrowCircleLeft } from "react-icons/fa";
 
 export default function Article({ srcImg, titleArticle, detailsArticle }) {
+
+    const [imageLoaded, setImageLoaded] = useState(false)
+    const onImageLoaded = () => setImageLoaded(true)
+
     return (
         <div className="col-12 article-card">
             <div className="article__card">
 
                 <div className="article-card__header">
-                    <a href="/" className="article-card__link-img">
+                    <Link to="/article-info" className="article-card__link-img">
                         <div className='blog__banner'>
-                            <img src={`Images/Blogs/${srcImg}`} alt="card" className="article-card__img" />
+                            <img
+                                src={`Images/Blogs/${srcImg}`}
+                                alt="card"
+                                className="article-card__img"
+                                onLoad={onImageLoaded}
+                            />
+                            {
+                                !imageLoaded &&
+                                <div className='parent-spinner'>
+                                    <Spinner animation="grow" variant="success" />
+                                </div>
+                            }
                         </div>
-                    </a>
+                    </Link>
                 </div>
 
                 <div className="article-card__content">
@@ -22,7 +39,7 @@ export default function Article({ srcImg, titleArticle, detailsArticle }) {
 
                 <p className="article-card__link-btn">
                     <hr />
-                    <a href="/" className='read-article'>مطالعه مقاله</a>
+                    <Link to="/article-info" className='read-article'>مطالعه مقاله</Link>
                     <FaArrowCircleLeft className='article-card__icon-arrow' />
                 </p>
 
