9293feacf7351837cca6e3152b3d7c196806cd1b
khadem-mh <khadem13359@gmail.com>
2024-04-04 01:06:45 +0330

    seprate function inputReducer for clean component and put the new file

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,66 +1,20 @@
 import React, { useReducer } from 'react'
 import './Input.css'
-import {
-    inputFullName, inputUserName, inputPhoneNumber, inputEmail, inputPassword
-} from "../../Types/TypesInput.js"
-//func
-import validateEmail from '../../Functions/ValidateEmail/ValidateEmail.js'
-
-const inputReducer = (state, action) => {
-    switch (action.type) {
-        case inputFullName: case inputUserName: {
-            return {
-                ...state,
-                value: action.value,
-                isValid: action.value.length > 6 ? true : false
-            }
-        }
-        case inputPhoneNumber: {
-            return {
-                ...state,
-                value: action.value,
-                isValid: action.value.length === 11 && action.value.slice(0, 2) === '09' ? true : false
-            }
-        }
-        case inputEmail: {
-            let isValidateEmail = validateEmail(action.value)
-            return {
-                ...state,
-                value: action.value,
-                isValid: isValidateEmail ? true : false
-            }
-        }
-        case inputPassword: {
-            return {
-                ...state,
-                value: action.value,
-                isValid: action.value.length >= 8 ? true : false
-            }
-        }
-        default: {
-            return state
-        }
-    }
-}
+import inputReducer from './InputReducer'
 
 export default function Input({ inpPlaceholder, inpIcon, type }) {
 
-    const [state, dispatch] = useReducer(inputReducer, {
-        value: '',
-        isValid: false
-    })
+    const [state, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
 
-    const inpChangeHandler = event => {
-        let inpValue = event.target.value
-        dispatch({
-            type,
-            value: inpValue,
-        })
+    const inpChangeHandler = event => dispatch({ type, value: event.target.value })
+
+    const inputFocusHandler = e => {
+        console.log(e)
     }
 
     return (
         <div className='user-datas__parent-input'>
-            <input type="text" placeholder={inpPlaceholder} value={state.value} onChange={event => inpChangeHandler(event)} />
+            <input type="text" placeholder={inpPlaceholder} value={state.value} onChange={event => inpChangeHandler(event)} onFocus={(e) => inputFocusHandler(e)} />
             {inpIcon}
         </div>
     )
---
+++ src/Components/Input/InputReducer.js
@@ -0,0 +1,44 @@
+import {
+    inputFullName, inputUserName, inputPhoneNumber, inputEmail, inputPassword
+} from "../../Types/TypesInput.js"
+//func
+import validateEmail from '../../Functions/ValidateEmail/ValidateEmail.js'
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case inputFullName: case inputUserName: {
+            return {
+                ...state,
+                value: action.value,
+                isValid: action.value.length > 6 ? true : false
+            }
+        }
+        case inputPhoneNumber: {
+            return {
+                ...state,
+                value: action.value,
+                isValid: action.value.length === 11 && action.value.slice(0, 2) === '09' ? true : false
+            }
+        }
+        case inputEmail: {
+            let isValidateEmail = validateEmail(action.value)
+            return {
+                ...state,
+                value: action.value,
+                isValid: isValidateEmail ? true : false
+            }
+        }
+        case inputPassword: {
+            return {
+                ...state,
+                value: action.value,
+                isValid: action.value.length >= 8 ? true : false
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+export default inputReducer
