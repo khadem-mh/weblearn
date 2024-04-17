eaa8321a84fbf09d5012dfd351a6c01cea136b98
khadem-mh <khadem13359@gmail.com>
2024-04-17 16:52:18 +0330

    coding section header page first account finish

--- src/Pages/MyAccount/PageFirstAccount/PageFirstAccount.jsx
+++ src/Pages/MyAccount/PageFirstAccount/PageFirstAccount.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import './PageFirstAccount.css'
 import StatusBoxAccount from '../Components/StatusBoxAccount/StatusBoxAccount'
 //icons
-import { HiOutlineCreditCard, HiOutlineRocketLaunch, HiOutlineTicket  } from "react-icons/hi2";
+import { HiOutlineCreditCard, HiOutlineRocketLaunch, HiOutlineTicket } from "react-icons/hi2";
 import { FaMoneyBillTrendUp } from "react-icons/fa6";
 
 //Funcs
@@ -9,11 +10,13 @@ import faNumber from '../../../Functions/FaNumber/FaNumber';
 
 export default function PageFirstAccount() {
   return (
-    <div>
-      <StatusBoxAccount bgColorBox={'#FACC15'} logo={<HiOutlineCreditCard />} title={'مجموع پرداخت ها'} subTitle={` ${faNumber(1165500)} تومان`} />
-      <StatusBoxAccount bgColorBox={'#4E81FB'} logo={<HiOutlineRocketLaunch />} title={'دوره های من'} subTitle={` ${faNumber(10)} دوره`} />
-      <StatusBoxAccount bgColorBox={'#F43F5E'} logo={<HiOutlineTicket  />} title={'مجموع تیکت ها'} subTitle={` ${faNumber(3)} تیکت`} />
-      <StatusBoxAccount bgColorBox={'#2ED573'} logo={<FaMoneyBillTrendUp />} title={'موجودی حساب'} subTitle={` 0 تومان`} />
+    <div className='account-page-first'>
+      <section className='account-page-first__header'>
+        <StatusBoxAccount bgColorBox={'#FACC15'} logo={<HiOutlineCreditCard />} title={'مجموع پرداخت ها'} subTitle={` ${faNumber(1165500)} تومان`} />
+        <StatusBoxAccount bgColorBox={'#4E81FB'} logo={<HiOutlineRocketLaunch />} title={'دوره های من'} subTitle={` ${faNumber(10)} دوره`} />
+        <StatusBoxAccount bgColorBox={'#F43F5E'} logo={<HiOutlineTicket />} title={'مجموع تیکت ها'} subTitle={` ${faNumber(3)} تیکت`} />
+        <StatusBoxAccount bgColorBox={'#2ED573'} logo={<FaMoneyBillTrendUp />} title={'موجودی حساب'} subTitle={` 0 تومان`} />
+      </section>
     </div>
   )
 }
