f41b376d7925153567dfcdb3f60174750928efd0
khadem-mh <khadem13359@gmail.com>
2024-06-14 14:17:51 -0700

    change ui search icon input

--- frontend/src/Components/SearchInput/SearchInput.jsx
+++ frontend/src/Components/SearchInput/SearchInput.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './SearchInput.css'
+import { Link } from 'react-router-dom'
 
-export default function SearchInput({w, h, fz, iconFz}) {
+export default function SearchInput({ w, h, fz, iconFz }) {
+
+    const [searchValInput, setSearchValInput] = useState('')
+
+    const handleSearchValInput = event => {
+        setSearchValInput(event.target.value)
+    }
+
+    const movePageSearch = event => {
+        if (searchValInput.length >= 3) {
+            return true
+        } else {
+            event.preventDefault()
+            return false
+        }
+    }
+
     return (
         <div className="parent-searchbar">
-            <div className="searchbar" style={{width: w, height: h}}>
-                <input type="text" className="search-input" style={{fontSize: fz}} placeholder="چیو میخوای یاد بگیری؟" />
-                <i className="fas fa-search search-icon" style={{fontSize: iconFz}}></i>
+            <div className="searchbar" style={{ width: w, height: h }}>
+                <input type="text" value={searchValInput} onChange={e => handleSearchValInput(e)} className="search-input" style={{ fontSize: fz }} placeholder="چیو میخوای یاد بگیری؟" />
+                <Link to={'/search'} onClick={e => movePageSearch(e)}>
+                    <i className="fas fa-search search-icon" style={{ fontSize: iconFz }}></i>
+                </Link>
             </div>
         </div>
     )
