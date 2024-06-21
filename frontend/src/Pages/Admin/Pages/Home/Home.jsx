11105bc70720af5e6231bed67c1398051fcfd745
khadem-mh <khadem13359@gmail.com>
2024-06-21 13:31:50 -0700

    modify fontSize panel admin

--- frontend/src/Pages/Admin/Pages/Home/Home.jsx
+++ frontend/src/Pages/Admin/Pages/Home/Home.jsx
@@ -37,7 +37,7 @@ export default function AdminPanelHome() {
   return (
     <div className='container-home-page'>
 
-      <h1 className='wellcom-back-admin-title'>خوش آمدید <strong>{adminInfos.firstname} {adminInfos.lastname}</strong> به پنل مدیریت</h1>
+      <h1 className='wellcom-back-admin-title'>خوش آمدید <strong>{adminInfos.name}</strong> به پنل مدیریت</h1>
 
       <section className='container-box box-info-small'>
 
