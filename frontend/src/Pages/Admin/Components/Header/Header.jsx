4a78daa1c0d28deb54acf73b1bdf94c1ae900c31
khadem-mh <khadem13359@gmail.com>
2024-06-21 20:37:37 -0700

    modify panel admin

--- frontend/src/Pages/Admin/Components/Header/Header.jsx
+++ frontend/src/Pages/Admin/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Header.css'
 import './media.css'
 import { Row, Col } from 'react-bootstrap'
@@ -7,7 +7,53 @@ import { BsBrightnessHigh } from 'react-icons/bs'
 import { IoSearch } from "react-icons/io5";
 import { IoMoonOutline } from "react-icons/io5";
 
-export default function Header({ children , isLightMode, setIsLightMode}) {
+export default function Header({ children, isLightMode, setIsLightMode }) {
+
+    const [isShowInput, setIsShowInput] = useState(false)
+    const searchBoxRef = useRef()
+
+    useEffect(() => {
+
+        function contrloWidthForShowSearchBox() {
+            if (window.innerWidth <= 992) {
+                searchBoxRef.current.classList.contains('show-search-icon') && searchBoxRef.current.classList.remove('show-search-icon')
+                searchBoxRef.current.classList.add('hidden-search-icon')
+            } else {
+                searchBoxRef.current.classList.contains('hidden-search-icon') && searchBoxRef.current.classList.remove('hidden-search-icon')
+                searchBoxRef.current.classList.add('show-search-icon')
+            }
+        }
+
+        contrloWidthForShowSearchBox()
+
+        window.addEventListener('resize', contrloWidthForShowSearchBox)
+        return () => window.removeEventListener('resize', contrloWidthForShowSearchBox)
+
+    }, [window.innerWidth])
+
+    useEffect(() => {
+
+        const handleClick = event => {
+            searchBoxRef.current.classList.remove('hidden-search-icon')
+            searchBoxRef.current.classList.add('show-search-icon')
+            let getPositionElem = searchBoxRef.current.getBoundingClientRect()
+
+            if (
+                event.clientY < getPositionElem.top ||
+                event.clientY > (getPositionElem.top + getPositionElem.height) ||
+                event.clientX > (getPositionElem.left + getPositionElem.width) ||
+                event.clientX < getPositionElem.left
+            ) {
+                searchBoxRef.current.classList.remove('show-search-icon')
+                searchBoxRef.current.classList.add('hidden-search-icon')
+            }
+
+        }
+
+        isShowInput && window.addEventListener('click', event => handleClick(event))
+        return () => window.removeEventListener('click', handleClick)
+
+    }, [isShowInput])
 
     const handleButtonClick = () => {
         setIsLightMode(prev => {
@@ -26,11 +72,12 @@ export default function Header({ children , isLightMode, setIsLightMode}) {
                     {children}
 
                     <Col className='header-left-section'>
-                        <button className='header-left-icon search-icon-nav d-auto d-md-none'>
+
+                        <button className={`header-left-icon search-icon-nav d-auto d-lg-none`} onClick={e => setIsShowInput(true)}>
                             <IoSearch className='header-icon' />
                         </button>
 
-                        <div className={`search-box`}>
+                        <div className="search-box" ref={searchBoxRef}>
                             <input type="text" placeholder='جست و جو بکنید ...' />
                             <button>جست و جو</button>
                         </div>
