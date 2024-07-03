b01bee678e0f826b2dd1fdc6725f3d2668f88c6a
khadem-mh <khadem13359@gmail.com>
2024-07-02 18:37:20 -0700

    handle logic form input for add new course

--- frontend/src/Pages/Admin/Components/AddNewProduct/AddNewProduct.jsx
+++ frontend/src/Pages/Admin/Components/AddNewProduct/AddNewProduct.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { SiNamecheap } from "react-icons/si";
+import { FaLess } from "react-icons/fa6";
 import InputEditModal from '../InputEditModal/InputEditModal';
 import { swal } from "sweetalert";
 
 export default function AddNewProduct({ getAllProducts }) {
 
-    const [objAddNewCourseInfos, setObjAddNewCourseInfos] = useState({
-        name: '',
-        description: '',
-        cover: null,
-        shortName: '',
-        price: '',
-        status: '',
-        categoryID: ''
-    })
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [cover, setCover] = useState(null)
+    const [shortName, setShortName] = useState('')
+    const [price, setPrice] = useState('')
+    const [status, setStatus] = useState('')
+    const [categoryID, setCategoryID] = useState('')
+    ///
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getAllCategories()
+    }, [])
+
+    const getAllCategories = () => {
+        fetch(`http://localhost:4000/v1/category`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
+            }
+        })
+            .then(res => res.json())
+            .then(datas => setCategories(datas))
+    }
+
+    const selectCategory = value => value !== -1 && setCategoryID(value)
+
+    const selectStatusCourse = value => value !== -1 && setStatus(value)
 
     const addNewCourse = e => {
         e.preventDefault()
@@ -23,12 +44,26 @@ export default function AddNewProduct({ getAllProducts }) {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
             },
-            body: JSON.stringify(objAddNewCourseInfos)
+            body: JSON.stringify({
+                name,
+                description,
+                cover,
+                shortName,
+                price,
+                status,
+                categoryID,
+            })
         })
             .then(res => res.ok ? res.json() : res.text().then(err => { return new Error(`Text Error: `, err) }))
             .then(() => {
                 getAllProducts('')
-                setObjAddNewCourseInfos({ name: '', description: '', cover: null, shortName: '', price: '', status: '', categoryID: '' })
+                setName("")
+                setDescription("")
+                setCover(null)
+                setShortName("")
+                setPrice("")
+                setStatus("")
+                setCategoryID("")
                 {
                     swal({
                         title: 'با موفقیت دوره اضافه شد',
@@ -56,17 +91,25 @@ export default function AddNewProduct({ getAllProducts }) {
 
             <form className='add-com-form'>
                 <div className='add-com-form-wrap'>
-                    <InputEditModal multiInp='name' valInp={objAddNewCourseInfos.name} setValInp={setObjAddNewCourseInfos} placeHolderInp='نام دوره' />
-                    <InputEditModal multiInp='price' valInp={objAddNewCourseInfos.price} setValInp={setObjAddNewCourseInfos} cildren='$$' placeHolderInp='قیمت دوره' />
+                    <InputEditModal multiInp='name' valInp={name} setValInp={setName} cildren={<SiNamecheap />} placeHolderInp='نام دوره' />
+                    <InputEditModal multiInp='price' valInp={price} setValInp={setPrice} cildren='$$' placeHolderInp='قیمت دوره' />
+                    <InputEditModal multiInp='shortName' valInp={shortName} setValInp={setShortName} cildren={<FaLess />} placeHolderInp='شورت نیم' />
                     <div className='mt-2'>
                         <label htmlFor="cover" className='text-secondary mb-2 me-2'>عکس دوره</label>
                         <input type="file" className="form-control" id='cover' />
                     </div>
-                    <select className="form-select border mt-md-5">
-                        <option selected valu="-1">دسته بندی دوره</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <select className="form-select border mt-md-5" onChange={event => selectCategory(event.target.value)}>
+                        <option value="-1">دسته بندی دوره</option>
+                        {
+                            categories.length && categories.map((category, index) => (
+                                <option key={index} value={category._id}>{category.title}</option>
+                            ))
+                        }
+                    </select>
+                    <select className="form-select border mt-md-5" onChange={event => selectStatusCourse(event.target.value)}>
+                        <option value="-1">وضعیت دوره</option>
+                        <option value="presell">پیش فروش</option>
+                        <option value="start">درحال برگزاری</option>
                     </select>
                 </div>
                 <button className='add-com-submit' onClick={event => addNewCourse(event)}>ثبت دوره</button>
--- frontend/src/Pages/Admin/Components/InputEditModal/InputEditModal.jsx
+++ frontend/src/Pages/Admin/Components/InputEditModal/InputEditModal.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import './InputEditModal.css'
 
-export default function InputEditModal({ cildren, placeHolderInp, valInp, setValInp, dis = false, multiInp = '' }) {
+export default function InputEditModal({ cildren, placeHolderInp, valInp, setValInp, dis = false }) {
 
     return (
         <div className='edit-products-form-group'>
             <span>
                 {cildren}
             </span>
-            <input type="text" minLength={3} disabled={dis} value={valInp || ""} onChange={e => multiInp.length >= 1
-                ?
-                setValInp(prev => { return { ...prev, multiInp: e.target.value } })
-                :
-                setValInp(e.target.value)} placeholder={placeHolderInp} className='edit-products-input' />
+            <input type="text" minLength={3} disabled={dis} value={valInp || ""} onChange={e => setValInp(e.target.value)} placeholder={placeHolderInp} className='edit-products-input' />
         </div>
     )
 }
