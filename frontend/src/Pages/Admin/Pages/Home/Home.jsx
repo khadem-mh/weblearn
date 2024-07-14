20d60dec326381fd33011134e537b21968fc1bdf
khadem-mh <khadem13359@gmail.com>
2024-07-13 18:29:42 -0700

    added files home in admin panel

--- frontend/src/Pages/Admin/Pages/Home/Home.jsx
+++ frontend/src/Pages/Admin/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './index.css'
 import './Home.css'
 import './media.css'
 
@@ -6,10 +7,76 @@ export default function AdminPanelHome() {
 
   let adminInfos = JSON.parse(localStorage.getItem('admin-infos'))
 
+  const [infos, setInfos] = useState([])
+  const [lastRegisteredUsers, setLastRegisteredUsers] = useState([])
+  const [adminName, setAdminName] = useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:4000/v1/infos/p-admin", {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token
+          }`,
+      },
+    })
+      .then((res) => res.json())
+      .then((pageInfo) => {
+        console.log(pageInfo);
+        setInfos(pageInfo.infos)
+        setLastRegisteredUsers(pageInfo.lastUsers)
+        setAdminName(pageInfo.adminName)
+      });
+  }, []);
+
   return (
     <div className='container-home-page'>
 
       <h1 className='wellcom-back-admin-title'>خوش آمدید <strong>{adminInfos.name}</strong> به پنل مدیریت</h1>
+
+      <div class="container-fluid" id="home-content">
+        <div class="container">
+          <div class="home-content-title">
+            <span class="welcome">
+              خوش آمدید,<span class="name">{adminName}</span>
+            </span>
+          </div>
+          <div class="home-content-boxes">
+            <div class="row">
+              {
+                infos.map(item => (
+                  <PAdminItem {...item} />
+                ))
+              }
+
+            </div>
+          </div>
+
+          <div class="home-content-latset-users">
+            <DataTable title="افراد اخیرا ثبت نام شده">
+
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>شناسه</th>
+                    <th>نام و نام خانوادگی</th>
+                    <th>ایمیل</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {lastRegisteredUsers.map((user, index) => (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{user.name}</td>
+                      {/* <td>09123443243</td> */}
+                      <td>{user.email}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
+            </DataTable>
+          </div>
+        </div>
+      </div>
 
     </div >
   )
