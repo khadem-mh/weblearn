7947b22866a554f583661f46415c189d6e6c12e3
khadem-mh <khadem13359@gmail.com>
2024-07-13 18:59:10 -0700

    remove file css junk

--- frontend/src/Pages/Admin/Pages/Home/Home.jsx
+++ frontend/src/Pages/Admin/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import './index.css'
 import './Home.css'
 import './media.css'
 import PAdminItem from '../../Components/PAdminItem/PAdminItem'
@@ -30,56 +29,46 @@ export default function AdminPanelHome() {
 
       <h1 className='wellcom-back-admin-title'>خوش آمدید <strong>{adminInfos.name}</strong> به پنل مدیریت</h1>
 
-      <div class="container-fluid" id="home-content">
-        <div class="container">
-          {/*  <div class="home-content-title">
-            <span class="welcome">
-              خوش آمدید,<span class="name">{adminName}</span>
-            </span>
-          </div> */}
-          <div class="home-content-boxes">
-            <div class="row">
-              {
-                infos.map(item => (
-                  <PAdminItem {...item} />
-                ))
-              }
-            </div>
-          </div>
-
-          <section class="home-content-latset-users">
-
-            <h1 className='products-title title-pr'>دوره ها</h1>
-            <div className='parent-table'>
-              {
-                <table>
-                  <thead>
-                    <tr>
-                      <th scope="col">شناسه</th>
-                      <th scope="col">نام و نام خانوادگی</th>
-                      <th scope="col">ایمیل</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {lastRegisteredUsers.map((user, index) => (
-                      <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{user.name}</td>
-                        {/* <td>09123443243</td> */}
-                        <td>{user.email}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-
-              }
-            </div>
-
-          </section>
+      <div class="home-content-boxes">
+        <div class="row">
+          {
+            infos.map(item => (
+              <PAdminItem {...item} />
+            ))
+          }
         </div>
       </div>
 
+      <section className="home-content-latset-users">
+
+        <h1 className='products-title title-pr'>دوره ها</h1>
+        <div className='parent-table'>
+          {
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">شناسه</th>
+                  <th scope="col">نام و نام خانوادگی</th>
+                  <th scope="col">ایمیل</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {lastRegisteredUsers.map((user, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{user.name}</td>
+                    {/* <td>09123443243</td> */}
+                    <td>{user.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+          }
+        </div>
+
+      </section>
     </div >
   )
 }
