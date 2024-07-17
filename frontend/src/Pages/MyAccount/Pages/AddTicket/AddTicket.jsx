577dd65e9630625da63ca5be675dcb2808f1b7eb
khadem-mh <khadem13359@gmail.com>
2024-07-17 15:49:33 -0700

    handle logic add tickets

--- frontend/src/Pages/MyAccount/Pages/AddTicket/AddTicket.jsx
+++ frontend/src/Pages/MyAccount/Pages/AddTicket/AddTicket.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+//import { Navigate } from 'react-router-dom'
 import './AddTicket.css'
+//import swal from 'sweetalert'
 
 export default function AddTicket() {
+
+  const [departments, setDepartments] = useState([])
+  const [departmentID, setDepartmentID] = useState(null)
+  const [departmentsSubs, setDepartmentsSubs] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/tickets/departments`)
+      .then(res => res.json())
+      .then(datas => setDepartments(datas))
+  }, [])
+
+  useEffect(() => {
+    if (departmentID && departmentID !== -1) {
+      fetch(`http://localhost:4000/v1/tickets/departments-subs/${departmentID}`)
+        .then(res => res.json())
+        .then(datas => setDepartmentsSubs(datas))
+    }
+  }, [departmentID])
+
   return (
     <div className='container-add-ticket'>
       <h2 className='add-ticket__title'>ارسال تیکت</h2>
       <hr />
       <p className='lable-task'>دپارتمان</p>
-      <select className='add-ticket__department'>
+      <select className='add-ticket__department' onChange={e => setDepartmentID(e.target.value)}>
         <option value="-1">دپارتمان مورد نظر...</option>
-        <option value="0">مدیریت</option>
-        <option value="1">مالی</option>
-        <option value="2">مشاوره</option>
+        {
+          departments && departments.map(({ title, _id }, index) => (
+            <option value={_id} key={index}>{title}</option>
+          ))
+        }
+      </select>
+
+      <p className='lable-task'>نوع تیکت</p>
+      <select className='add-ticket__department'>
+        <option value="-1">نوع تیکت را انتخاب کنید...</option>
+        {
+          departmentsSubs && departmentsSubs.map(({ title, _id }, index) => (
+            <option value={_id} key={index}>{title}</option>
+          ))
+        }
       </select>
 
       <form className='add-ticket__form'>
