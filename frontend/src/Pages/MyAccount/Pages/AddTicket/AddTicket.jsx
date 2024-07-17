9062fec46ba84b4a052ca28fbbeb91673f5fc292
khadem-mh <khadem13359@gmail.com>
2024-07-17 16:22:03 -0700

    handle logic users courses

--- frontend/src/Pages/MyAccount/Pages/AddTicket/AddTicket.jsx
+++ frontend/src/Pages/MyAccount/Pages/AddTicket/AddTicket.jsx
@@ -8,11 +8,25 @@ export default function AddTicket() {
   const [departments, setDepartments] = useState([])
   const [departmentID, setDepartmentID] = useState(null)
   const [departmentsSubs, setDepartmentsSubs] = useState([])
+  const [usersCourses, setUsersCourses] = useState([])
 
   useEffect(() => {
     fetch(`http://localhost:4000/v1/tickets/departments`)
       .then(res => res.json())
       .then(datas => setDepartments(datas))
+
+    fetch(`http://localhost:4000/v1/users/courses`, {
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).token}`,
+      }
+    })
+      .then(res => res.json())
+      .then(datas => {
+        console.log(datas);
+        setUsersCourses(datas)
+      })
+
   }, [])
 
   useEffect(() => {
@@ -41,8 +55,18 @@ export default function AddTicket() {
       <select className='add-ticket__department'>
         <option value="-1">نوع تیکت را انتخاب کنید...</option>
         {
-          departmentsSubs && departmentsSubs.length && departmentsSubs.map(({ title, _id }, index) => (
-            <option value={_id} key={index}>{title}</option>
+          departmentsSubs && departmentsSubs.length && departmentsSubs.map((item, index) => (
+            <option value={item._id} key={index}>{item.title}</option>
+          ))
+        }
+      </select>
+
+      <p className='lable-task'>دوره مدنظر</p>
+      <select className='add-ticket__department'>
+        <option value="-1">دوره مدنظر را انتخاب کنید...</option>
+        {
+          usersCourses && usersCourses.length && usersCourses.map((item, index) => (
+            <option value={item.course._id} key={index}>{item.course.name}</option>
           ))
         }
       </select>
