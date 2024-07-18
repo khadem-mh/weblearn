28e820d9112a5cde0bc3b58528722edd47d031e9
khadem-mh <khadem13359@gmail.com>
2024-07-18 16:09:01 -0700

    add hook state to page details account

--- frontend/src/Pages/MyAccount/Pages/DetailsAccount/DetailsAccount.jsx
+++ frontend/src/Pages/MyAccount/Pages/DetailsAccount/DetailsAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './DetailsAccount.css'
 import { VscGitPullRequestGoToChanges } from "react-icons/vsc";
 import { AuthContext } from '../../../../Contexts/AuthContext';
@@ -6,6 +6,11 @@ import { AuthContext } from '../../../../Contexts/AuthContext';
 export default function DetailsAccount() {
 
   const authContext = useContext(AuthContext)
+  const [name, setName] = useState(authContext.userInfos.name)
+  const [username, setUserName] = useState(authContext.userInfos.username)
+  const [email, setEmail] = useState(authContext.userInfos.email)
+  const [password, setPassword] = useState("")
+  const [phone, setPhone] = useState(authContext.userInfos.phone)
 
   return (
     <section className='details-account'>
@@ -15,7 +20,7 @@ export default function DetailsAccount() {
         <h2>جزئیات حساب کاربری</h2>
         <hr />
 
-        <section className='details-account__parent'>
+        <section className='details-account__parent py-4'>
           <div className='right-box__right'>
 
             <div className='right-box__right-prof'>
@@ -26,11 +31,11 @@ export default function DetailsAccount() {
             </div>
 
             <p className='lable-task'>شماره موبایل</p>
-            <input type="text" className='input-details-account' defaultValue={authContext.userInfos.phone} />
+            <input type="text" className='input-details-account' defaultValue={phone} />
             <p className='lable-task'>نام</p>
-            <input type="text" className='input-details-account' defaultValue={authContext.userInfos.name} />
+            <input type="text" className='input-details-account' defaultValue={name} />
             <p className='lable-task'>نام کاربری</p>
-            <input type="text" className='input-details-account' defaultValue={authContext.userInfos.username} />
+            <input type="text" className='input-details-account' defaultValue={username} />
 
           </div>
 
@@ -38,13 +43,11 @@ export default function DetailsAccount() {
             <p className='lable-task'>تاریخ ثبت نام</p>
             <input type="text" className='input-details-account' defaultValue={authContext.userInfos.createdAt && authContext.userInfos.createdAt.slice(0, 10)} />
             <p className='lable-task'>ایمیل</p>
-            <input type="text" className='input-details-account' defaultValue={authContext.userInfos.email} />
+            <input type="text" className='input-details-account' defaultValue={email} />
           </div>
         </section>
 
-        <div className='btn-details-account-parent'>
-          <button className='btn-details-account'>ثبت اطلاعات</button>
-        </div>
+
 
       </section>
 
@@ -55,12 +58,12 @@ export default function DetailsAccount() {
         <input type="text" placeholder='رمز فعلی را وارد کنید' className='input-details-account' />
         <p className='forget-password-details-account'>رمز عبور را فراموش کرده اید؟</p>
         <p className='lable-task'>رمز عبور جدید</p>
-        <input type="text" placeholder='رمز جدید را وارد کنید' className='input-details-account' />
-        <div className='btn-details-account-parent'>
-          <button className='btn-details-account'>تغیر رمز</button>
-        </div>
+        <input type="text" placeholder='رمز جدید را وارد کنید' className='input-details-account' value={password} />
+        <br />
       </section>
-
+      <div className='btn-details-account-parent'>
+        <button className='btn-details-account'>بروزرسانی اطلاعات</button>
+      </div>
     </section>
   )
 }
