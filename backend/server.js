07f8315aaa8f83f58e8357f09181c932f964f706
khadem-mh <khadem13359@gmail.com>
2024-07-28 11:22:07 -0700

    fix bug handle backend

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,16 @@ const app = require('./app');
 
 //* Load env
 dotenv.config();
-//process.env.MONGO_URI
+
 //* Database connection
 (async () => {
   try {
-    const conn = await mongoose
-      .connect(
-        `mongodb://root:9ItkwjL44GUymIiHArYTKnis@weblearning-db:27017/weblearning-db?
-        authSource=admin`,
-        {
-          useNewUrlParser: true,
-          authSource: "admin"
-        }
-      )
-      .then(() => console.log('DB Conected!'))
+    const conn = await mongoose.connect(`mongodb://root:9ItkwjL44GUymIiHArYTKnis@weblearning-db:27017/weblearning-db?
+      authSource=admin`,
+      {
+        useNewUrlParser: true,
+        authSource: "admin"
+      });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     //?error catch
@@ -31,5 +27,5 @@ const port = +process.env.PORT || 3000;
 
 const productionMode = process.env.NODE_ENV === 'production'
 app.listen(port, () => {
-  console.log(`Server running in ${productionMode ? "production" : "development"} mode on port ${port}`);
+  console.log(`Server running in ${productionMode?"production":"development"} mode on port ${port}`);
 });
